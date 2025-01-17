63da9c2011effa3dd498697c197e0708658c6e36
jaeyyyyy <145132400+jaeyyyyy@users.noreply.github.com>
2025-01-17 16:09:16 +0900

    Update 중간부터 다시 크롤링.py

--- python/중간부터 다시 크롤링.py
+++ python/중간부터 다시 크롤링.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.ie.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 import time
-import random
-import re
 import pandas as pd
 
 # 왼쪽 프레임으로 전환
